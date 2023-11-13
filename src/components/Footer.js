c001f1bcfa0ace747e3e6694d49b0a822eca52ff
EndrXOriginial <chris.pin@hotmail.ca>
2023-11-13 09:54:10 -0500

    added Footer & Header both finished

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className='footer'>
+                <p className='footer-p'>This is a non-commercial website using the Facts API, shibe.online API, Bored API and Current Weather Data API</p>
+            </footer>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {  NavLink  } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <div>
+            <nav className="nav-header">
+                <h1 className="title-page">Boredom</h1>
+            </nav>
+        </div>
+    )
+}
